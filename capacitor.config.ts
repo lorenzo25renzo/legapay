9833d1410480c9f4b830fff807cd35634ca919eb
Lorenzo Tabada <your_email@example.com>
2026-04-13 23:09:47 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'legapayApp',
+  webDir: 'www'
+};
+
+export default config;
